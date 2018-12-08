388f4702f59a445050b89aab42630ccaf5cb0fd7
4tronix <Sales@4tronix.co.uk>
2018-12-08 16:12:08 +0000

    Remove Pullups

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ namespace bitcommander {
 
     // mount buttons on the pins with a pullup mode
     // TODO: fix this issue in the DAL itself
-#define ALLOC_PIN_BUTTON(id) new MicroBitButton(getPin(id)->name, id, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
+#define ALLOC_PIN_BUTTON(id) new MicroBitButton(getPin(id)->name, id, MICROBIT_BUTTON_ALL_EVENTS);
     ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P12)
     ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P16)
     ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P14)
